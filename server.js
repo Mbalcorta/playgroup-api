d730b5cbc0c6c5836e4738464c1dbc0c6afd95a6
Mateo Balcorta <misterrattail@gmail.com>
2018-05-21 15:22:56 -0700

    Add express server.

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var express = require(‘express’);
-var app = express();
+const express = require('express')
+const PythonShell = require('python-shell');
+
+const app = express();
+
+app.get('/', callEventData);
+
+function callEventData(req, res) {
+  PythonShell.run('./eventsData.py', function (err, data) {
+    if (err) res.send(err);
+    res.send(data.toString())
+  });
+}
 
 app.listen(3000, function () {
-  console.log(‘server running on port 3000’);
+  console.log('server running on port 3000')
 })
