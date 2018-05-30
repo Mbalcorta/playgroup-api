59d4210d2fd07404292ac817cc3b08692e2d5ac1
Mateo Balcorta <misterrattail@gmail.com>
2018-05-30 08:43:10 -0700

    add firebase and data to db.

--- server.js
+++ server.js
@@ -1,19 +1,46 @@
-const express = require('express')
-const PythonShell = require('python-shell');
+var firebase = require("firebase-admin");
+var PythonShell = require('python-shell');
 
-const app = express();
+var serviceAccount = require("./serviceAccountKey.json");
 
-app.get('/', callEventData);
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://tag-app-8372b.firebaseio.com"
+});
 
-function callEventData(req, res) {
-  PythonShell.run('./eventsData.py', function (err, data) {
-    if (err) res.send(err);
-    res.send(data.toString())
-  });
-}
+var db = firebase.database();
 
-const port = process.env.PORT || 3000;
+var myPythonScriptPath = 'eventsData.py';
 
-app.listen(port, function () {
-  console.log('server running on port 3000')
-})
+// Use python shell
+
+var pyshell = new PythonShell(myPythonScriptPath);
+
+
+pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    const eventsPath = db.ref('libraryEvents');
+    const events = JSON.stringify(message)
+    eventsPath.set(events);
+});
+
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+    if (err){
+        throw err;
+    };
+
+    console.log('finished');
+});
+
+// var usersRef = ref.child("users");
+// usersRef.set({
+//   alanisawesome: {
+//     date_of_birth: "June 23, 1912",
+//     full_name: "Alan Turing"
+//   },
+//   gracehop: {
+//     date_of_birth: "December 9, 1906",
+//     full_name: "Grace Hopper"
+//   }
+// });
