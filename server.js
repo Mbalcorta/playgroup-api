e6979eb3de7f2c5433cfde5cd32f2b1f55980f6c
Mateo Balcorta <misterrattail@gmail.com>
2018-06-08 15:57:04 -0700

    Clean up.

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ firebase.initializeApp({
 
 var db = firebase.database();
 
-var myPythonScriptPath = 'eventsData.py';
-
-var pyshell = new PythonShell(myPythonScriptPath);
+const myPythonScriptPath = 'eventsData.py';
+const pyshell = new PythonShell(myPythonScriptPath);
 const allEventsArray = []
 
 //figure out way to trigger cron job at midnight for this to pull data from sites everyday
@@ -23,14 +22,6 @@ pyshell.on('message', function (message) {
     eventsPath.set(allEvents)
   });
 
-// db.ref().on("value", function(snapshot) {
-//   const events = snapshot.val()
-//   console.log(events['libraryEvents']['melrose_library']['location'])
-// }, function (error) {
-//   console.log("Error: " + error.code);
-// });
-
-// end the input stream and allow the process to exit
 pyshell.end(function (err) {
     if (err){
         throw err;
@@ -38,15 +29,3 @@ pyshell.end(function (err) {
     console.log('finished');
     process.exit()
 });
-
-// var usersRef = ref.child("users");
-// usersRef.set({
-//   alanisawesome: {
-//     date_of_birth: "June 23, 1912",
-//     full_name: "Alan Turing"
-//   },
-//   gracehop: {
-//     date_of_birth: "December 9, 1906",
-//     full_name: "Grace Hopper"
-//   }
-// });
