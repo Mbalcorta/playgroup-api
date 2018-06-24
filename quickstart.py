32608f3dbc9beb281c661c57282235e521123f70
Mateo Balcorta <misterrattail@gmail.com>
2018-06-24 15:48:46 -0700

    Update date format for allendale events.

--- quickstart.py
+++ quickstart.py
@@ -43,7 +43,7 @@ def main(argv):
                 def custom_strftime(format, t):
                     return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
                 
-                dateFormatted = custom_strftime('%A %B {S}, %Y', objDate)
+                dateFormatted = custom_strftime('%A, %B {S}, %Y', objDate)
                
                 allEvents.append({
                     'date': dateFormatted,
